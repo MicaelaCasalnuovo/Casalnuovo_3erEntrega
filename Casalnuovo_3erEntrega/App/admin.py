cddb1d77a6e309c3c6cdd0f6e75752ea2886babe
micaela casalnuovo <mica.casalnuovo@gmail.com>
2024-12-22 20:16:33 -0300

    Creado el superusuario y se cargaron datos desde la pagina

--- Casalnuovo_3erEntrega/App/admin.py
+++ Casalnuovo_3erEntrega/App/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from App.models import ModeloCliente
+from App.models import ModeloProducto
+from App.models import ModeloReseña
+from App.models import ModeloCompra
 
 # Register your models here.
+admin.site.register(ModeloCliente)
+admin.site.register(ModeloProducto)
+admin.site.register(ModeloReseña)
+admin.site.register(ModeloCompra)
